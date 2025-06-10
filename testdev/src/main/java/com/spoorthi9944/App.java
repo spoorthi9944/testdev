0b48aa785cc1b7f8149c490ce4747585b596c66f
unknown <spoorthinn@gmail.com>
2025-06-11 00:12:31 +0530

    first commit

---
+++ testdev/src/main/java/com/spoorthi9944/App.java
@@ -0,0 +1,13 @@
+package com.spoorthi9944;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
